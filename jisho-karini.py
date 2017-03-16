9e161df23f98facbeaf0981e3e4770cdcf48729b
eliasrodrigo <eliasrodrigo@protonmail.com>
2017-03-16 14:27:32 +0100

    print only 'japanese{}' and 'english_definitions'

--- jisho-karini.py
+++ jisho-karini.py
@@ -5,15 +5,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-j", help="Open a browser and show the result", nargs=1)
 args = parser.parse_args()
 
-argJ = args.j[0] + '"'
+argJ = args.j[0] + '\"'
 
 def fetching():
-    url = 'http://jisho.org/api/v1/search/words?keyword="'
-    response = requests.get(url, argJ)
+    url = 'http://jisho.org/api/v1/search/words?keyword=\"' + argJ
+    response = requests.get(url,)
     response.raise_for_status()
     termData = json.loads(response.text)
-    #t = termData['list']
-    print(termData)
+    t = termData['data']
+    td = t[0]
+
+    tr = td['senses'] 
+    eng = tr[0]
+    print(td['japanese'])
+    print(eng['english_definitions'])
 
 if args.j:
     fetching()
