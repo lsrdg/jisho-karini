5ccde15b99093b852559dd665cce063453426f18
eliasrodrigo <eliasrodrigo@protonmail.com>
2017-03-19 21:08:15 +0100

    add '-b' and 'menuInit()'

--- jisho-karini.py
+++ jisho-karini.py
@@ -1,24 +1,46 @@
-import requests, argparse, json
+import requests, argparse, json, webbrowser
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-j", help="Open a browser and show the result", nargs=1)
+parser.add_argument("-b", help="Open a browser and show the result", nargs=1)
 args = parser.parse_args()
 
-argJ = args.j[0] + '\"'
+
 
 def fetching():
+    argJ = args.j[0] + '\"'
     url = 'http://jisho.org/api/v1/search/words?keyword=\"' + argJ
-    response = requests.get(url,)
+    response = requests.get(url)
     response.raise_for_status()
     termData = json.loads(response.text)
     t = termData['data']
-    td = t[0]
 
-    tr = td['senses'] 
-    eng = tr[0]
-    print(td['japanese'])
-    print(eng['english_definitions'])
+    for item in t:
+        
+        print(item['japanese'])
+        print(item['senses'], '\n')
 
-if args.j:
-    fetching()
+# ------- print [0]
+#    td = t[0]
+#
+#    tr = td['senses'] 
+#    eng = tr[0]
+#    print(td['japanese'])
+#    print(eng['english_definitions'])
+
+def browser():
+    argB = args.b[0]
+    webbrowser.open('http://jisho.org/search/' + argB, new=2)
+
+def menuInit():
+    if args.j:
+        fetching()
+
+    elif args.b:
+        browser()
+
+    else:
+        print("Oooops!")
+
+menuInit()
